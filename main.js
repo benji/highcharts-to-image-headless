973e0bc3e85c33a65205789463c8889ff1208a7d
Benjamin Jaton <benjaminjaton@asana.com>
2020-06-20 17:43:41 -0700

    cleanup

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ const path = require("path");
 const chart2image = require("./chart2image.js");
 const chart1 = require("./chart1.js");
 
-const templatePath = "file://" + path.resolve(__dirname, "template.html");
-const domElementId = "chart";
-
 chart2image({
   chartConfig: chart1,
   saveToFile: "chart.png",
-  templatePath,
-  domElementId,
+  templatePath: "file://" + path.resolve(__dirname, "template.html"),
+  domElementId: "chart",
 });
